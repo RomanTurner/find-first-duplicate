e43f88b8743c25724e25fb2149f60764c4490fe6
Roman Turner <rn.turner@live.com>
2021-03-02 13:54:24 -0800

    assignment complete

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,6 +1,21 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  let mySet1 = new Set;
+  for (let i in arr) {
+    mySet1.add(arr[i]);
+  }
+  if (mySet1.size != arr.length) {
+    return arr[mySet1.size];
+  } else {
+    return -1;
+  };
 }
+
+
+console.log(findFirstDuplicate([2]));
+console.log(findFirstDuplicate([2, 1, 3, 2, 1, 3, 3, 2]));
+console.log(findFirstDuplicate([1, 2, 3, 4]));
+console.log(findFirstDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 8]));
+
 
 if (require.main === module) {
   // add your own tests in here
